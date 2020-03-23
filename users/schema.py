ef4bc8cb36b8681b3779c98999b4692749c5e453
abheist <abhishek291093@gmail.com>
2020-03-23 16:33:21 +0530

    feature | user authentication | added mutation for creating user

--- users/schema.py
+++ users/schema.py
@@ -1,16 +1,46 @@
 import graphene
+from django.contrib.auth import get_user_model
 from graphene_django.types import DjangoObjectType
-
-from users.models.user import User
 
 
 class UserType(DjangoObjectType):
     class Meta:
-        model = User
+        model = get_user_model()
 
 
 class Query(object):
-    all_users = graphene.List(UserType)
+    users = graphene.List(UserType)
+    me = graphene.Field(UserType)
 
-    def resolve_all_users(self, info, **kwargs):
-        return User.objects.all()
+    def resolve_users(self, info, **kwargs):
+        return get_user_model().objects.all()
+
+    def resolve_me(self, info, **kwargs):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('Not logged in!')
+
+        return user
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    def mutate(self, info, username, email, password):
+        user = get_user_model()(
+                username=username,
+                email=email
+        )
+        user.set_password(password)
+        user.save()
+
+        return CreateUser(user=user)
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
