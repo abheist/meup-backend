95f52a39ac0037e75a8766a2ca898c4419ded382
abheist <abhishek291093@gmail.com>
2020-03-26 19:14:33 +0530

    fix | added auth mutations | register, verify_account, token_auth, update_account, resend_activation_email, send_password_reset_email, send_secondary_email_activation, verify_secondary_email, swap_email, remove_secondary_email, archive_account, delete_account, password_reset, password_change,, verify_token, refresh_token, revoke_token

--- meupBackend/schema.py
+++ meupBackend/schema.py
@@ -1,17 +1,35 @@
 import graphene
-import graphql_jwt
+from graphql_auth import mutations
 
 import users.schema
+
+
+class AuthMutation(graphene.ObjectType):
+    register = mutations.Register.Field()
+    verify_account = mutations.VerifyAccount.Field()
+    token_auth = mutations.ObtainJSONWebToken.Field()
+    update_account = mutations.UpdateAccount.Field()
+    resend_activation_email = mutations.ResendActivationEmail.Field()
+    send_password_reset_email = mutations.SendPasswordResetEmail.Field()
+    send_secondary_email_activation = mutations.SendSecondaryEmailActivation.Field()
+    verify_secondary_email = mutations.VerifySecondaryEmail.Field()
+    swap_email = mutations.SwapEmails.Field()
+    remove_secondary_email = mutations.RemoveSecondaryEmail.Field()
+    archive_account = mutations.ArchiveAccount.Field()
+    delete_account = mutations.DeleteAccount.Field()
+    password_reset = mutations.PasswordReset.Field()
+    password_change = mutations.PasswordChange.Field()
+    verify_token = mutations.VerifyToken.Field()
+    refresh_token = mutations.RefreshToken.Field()
+    revoke_token = mutations.RevokeToken.Field()
 
 
 class Query(users.schema.Query, graphene.ObjectType):
     pass
 
 
-class Mutation(users.schema.Mutation, graphene.ObjectType):
-    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
-    verify_token = graphql_jwt.Verify.Field()
-    refresh_token = graphql_jwt.Refresh.Field()
+class Mutation(AuthMutation, graphene.ObjectType):
+    pass
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
