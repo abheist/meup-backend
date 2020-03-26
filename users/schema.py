84ea9b8356e2b65a5380bee6456f81a89ce196f9
abheist <abhishek291093@gmail.com>
2020-03-26 19:13:02 +0530

    refactored | removed custom createUser mutation

--- users/schema.py
+++ users/schema.py
@@ -21,27 +21,3 @@ class Query(object):
     def resolve_me(self, info, **kwargs):
         user = info.context.user
         return user
-
-
-class CreateUser(graphene.Mutation):
-    user = graphene.Field(UserType)
-
-    class Arguments:
-        username = graphene.String(required=True)
-        email = graphene.String(required=True)
-        password = graphene.String(required=True)
-
-    @login_required
-    def mutate(self, info, username, email, password):
-        user = get_user_model()(
-                username=username,
-                email=email
-        )
-        user.set_password(password)
-        user.save()
-
-        return CreateUser(user=user)
-
-
-class Mutation(graphene.ObjectType):
-    create_user = CreateUser.Field()
