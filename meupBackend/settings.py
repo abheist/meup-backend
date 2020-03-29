a13513da6ab53f8f79a40657455b3a120b39b48d
abheist <abhishek291093@gmail.com>
2020-03-29 19:12:38 +0530

    🥱 Overridden email templates
    Signed-off-by: abheist <abhishek291093@gmail.com>

--- meupBackend/settings.py
+++ meupBackend/settings.py
@@ -52,7 +52,7 @@ CORS_ORIGIN_WHITELIST = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
