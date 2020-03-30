9a5e7fd6727ff59cd1d174f1fe17d12b81b0d0b3
abheist <abhishek291093@gmail.com>
2020-03-30 20:09:13 +0530

    a bit of refactoring
    Signed-off-by: abheist <abhishek291093@gmail.com>

--- meupBackend/schema.py
+++ meupBackend/schema.py
@@ -7,18 +7,15 @@ import users.schema
 class AuthMutation(graphene.ObjectType):
     register = mutations.Register.Field()
     verify_account = mutations.VerifyAccount.Field()
-    password_reset = mutations.PasswordReset.Field()
     token_auth = mutations.ObtainJSONWebToken.Field()
+
+    send_password_reset_email = mutations.SendPasswordResetEmail.Field()
+    password_reset = mutations.PasswordReset.Field()
+
     update_account = mutations.UpdateAccount.Field()
     resend_activation_email = mutations.ResendActivationEmail.Field()
-    send_password_reset_email = mutations.SendPasswordResetEmail.Field()
-    send_secondary_email_activation = mutations.SendSecondaryEmailActivation.Field()
-    verify_secondary_email = mutations.VerifySecondaryEmail.Field()
-    swap_email = mutations.SwapEmails.Field()
-    remove_secondary_email = mutations.RemoveSecondaryEmail.Field()
-    archive_account = mutations.ArchiveAccount.Field()
-    delete_account = mutations.DeleteAccount.Field()
     password_change = mutations.PasswordChange.Field()
+
     verify_token = mutations.VerifyToken.Field()
     refresh_token = mutations.RefreshToken.Field()
     revoke_token = mutations.RevokeToken.Field()
